77888120d3fb276ddbfc820bdf045d3c73304d62
eduardog <46846973+featjinsoul@users.noreply.github.com>
2023-02-21 17:19:32 -0600

    見てをしないで。It can barely automate the whole thing now

--- auto_generate_all.py
+++ auto_generate_all.py
@@ -19,24 +19,11 @@ farc_exists = farc_spec is not None
 spr_spec = importlib.util.find_spec("spr")
 spr_exists = spr_spec is not None
 
-if farc_exists and spr_exists:
-    import farc
-    import spr
-    def replace_sprite_texture(farc_to_read, sprite_binary_to_edit, texname_to_edit, image_to_use, farc_to_write):
-        entries = farc.read(farc_to_read)
-        sprset = spr.read_from_raw(entries[sprite_binary_to_edit])
-        sprset.replace_texture(texname_to_edit, image_to_use)
-        farc.save(entries, farc_to_write, True)
-    var_replace_sprite = None
-else:
-    print("farc and spr are not present...")
-    print("not automating sprite creation!")
-
 root = tk.Tk()
 root.withdraw()
 # scuffed but this works
-tk.messagebox.showwarning(message='The autogenerate script will generate the 4 fonts required for the base game. Default metrics and sizing will be used.')
-tk.messagebox.showwarning(message='Please select the regular fonts.')
+print('The autogenerate script will generate the 4 fonts required for the base game. Default metrics and sizing will be used.')
+print('Please select the regular fonts.')
 regular_fontlist = fd.askopenfilenames(filetypes=[("Fonts", ".ttf .otf")], parent=root, title='Choose the regular fonts.')
 regular_fontlist = list(regular_fontlist)
 for file in regular_fontlist:
@@ -48,7 +35,7 @@ elif platform == "win32":
     regular_fontlist = regular_fontlist.replace('(','').replace(')','').replace('[','').replace(']','').replace(', ',',').replace("'",'')
 
 # time to do it again for the bold fonts.
-tk.messagebox.showwarning(message='Please select the bold fonts.')
+print("Please select the bold fonts.")
 bold_fontlist = fd.askopenfilenames(filetypes=[("Fonts", ".ttf .otf")], parent=root, title='Choose the bold fonts.')
 bold_fontlist = list(bold_fontlist)
 for file in bold_fontlist:
@@ -60,6 +47,24 @@ elif platform == "win32":
     bold_fontlist = bold_fontlist.replace('(','').replace(')','').replace('[','').replace(']','').replace(', ',',').replace("'",'')
 print(regular_fontlist)
 print(bold_fontlist)
+if farc_exists and spr_exists:
+    import farc
+    import spr
+    def replace_sprite_texture(farc_to_read, sprite_binary_to_edit, texname_to_edit, image_to_use, farc_to_write):
+        entries = farc.read(farc_to_read)
+        sprset = spr.read_from_raw(entries[sprite_binary_to_edit])
+        sprset.replace_texture(texname_to_edit, image_to_use)
+        farc.save(entries, farc_to_write, True)
+    var_replace_sprite = None
+    import json
+    mod_json_array = {
+          "preview": "https://images.gamebanana.com/img/ss/mods/62db8b8e73794.jpg","submitter": "feat_jinsoul","avi": "https://images.gamebanana.com/img/av/62bbea45f31d1.png","upic": "","caticon": "https://images.gamebanana.com/img/ico/ModCategory/6297a8ca8644e.png","cat": "User Interface","description": "","homepage": "https://gamebanana.com/mods/391229","lastupdate": "2022-07-23T05:50:18"
+    }
+    json_data = json.dumps(mod_json_array)
+    folder_name = input("Please enter a folder name for the output. ")
+else:
+    print("farc and spr are not present...")
+    print("not automating sprite creation!")
 
 print("Running scripts..")
 print("Your platform is {}...".format(platform))
@@ -115,3 +120,49 @@ if var_replace_sprite == True:
     os.remove("font14_36x36.png")
     os.remove("font20_36x36.png")
     os.remove("font22_36x36.png")
+print("Setting up mod folder..")
+toml_array = """enabled = true
+name = "IBM Plex® Fonts"
+description = "This is a mod that replaces both the LATIN9 and default (JP) fontsets. Does not include support for bold fonts at this time."
+version = "1.0"
+date = "22.07.2022"
+author = "feat_jinsoul"
+include = ["."]"""
+try:
+    if not os.path.isdir(folder_name):
+        rom_folder_name = folder_name + "/rom"
+        auth2d_folder_name = rom_folder_name + "/2d"
+        os.makedirs(folder_name)
+        os.makedirs(rom_folder_name)
+        os.makedirs(auth2d_folder_name)
+except OSError as e:
+    print(e)
+    print("Unable to create the directories for the folder name you want. Exiting...")
+    exit(1)
+# ok now I know the directories exist because I made them!
+try:
+    os.rename("spr_fnt_36latin9.farc", auth2d_folder_name + "/spr_fnt36_latin9.farc")
+    os.rename("spr_fnt_36.farc", auth2d_folder_name + "/spr_fnt_36.farc")
+    os.rename("spr_fnt_bold36.farc", auth2d_folder_name + "/spr_fnt_bold36.farc")
+    os.rename("spr_fnt_bold36latin9.farc", auth2d_folder_name + "/spr_fnt_bold36latin9.farc")
+except OSError as e:
+    print(e)
+    print("Unable to move the sprites to their location. Exiting...")
+    exit(1)
+# ok time to move the fontmap
+try:
+    os.rename("fontmap.farc", rom_folder_name + "/fontmap.farc")
+except OSError as e:
+    print(e)
+    print("Unable to move the fontmap to its location. Exiting...")
+    exit(1)
+# ok time to. um. make the json and the toml
+try:
+    with open("{}\\config.toml".format(folder_name), "a+", encoding="utf-8") as f:
+        f.write(toml_array)
+    with open("{}\\mod.json".format(folder_name), "a+", encoding="utf-8") as f2:
+        f2.write(json_data)
+except OSError as e:
+    print(e)
+    print("Unable to create the config.toml. Exiting...")
+    exit(1)
