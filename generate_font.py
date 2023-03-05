64524d2067266a7659fceaa1ca905ae2202b3480
feat_jinsoul <46846973+featjinsoul@users.noreply.github.com>
2023-03-04 22:04:52 -0600

    Refactor: move automation into generate_font

--- generate_font.py
+++ generate_font.py
@@ -2,8 +2,33 @@ from sys import exit
 from PIL import Image, ImageDraw, ImageFont
 from math import ceil
 from os.path import join as joinpath
+import os
 import json
 import argparse
+import importlib.util
+
+farc_spec = importlib.util.find_spec("farc")
+farc_exists = farc_spec is not None
+spr_spec = importlib.util.find_spec("spr")
+spr_exists = spr_spec is not None
+
+if farc_exists and spr_exists:
+    import farc
+    import spr
+    def replace_sprite_texture(farc_to_read, sprite_binary_to_edit, texname_to_edit, image_to_use, farc_to_write):
+        entries = farc.read(farc_to_read)
+        sprset = spr.read_from_raw(entries[sprite_binary_to_edit])
+        sprset.replace_texture(texname_to_edit, image_to_use)
+        farc.save(entries, farc_to_write, True)
+    var_replace_sprite = None
+    import json
+    mod_json_array = {
+          "preview": "https://images.gamebanana.com/img/ss/mods/62db8b8e73794.jpg","submitter": "feat_jinsoul","avi": "https://images.gamebanana.com/img/av/62bbea45f31d1.png","upic": "","caticon": "https://images.gamebanana.com/img/ico/ModCategory/6297a8ca8644e.png","cat": "User Interface","description": "","homepage": "https://gamebanana.com/mods/391229","lastupdate": "2022-07-23T05:50:18"
+    }
+    json_data = json.dumps(mod_json_array)
+else:
+    print("farc and spr are not present...")
+    print("not automating sprite creation!")
 
 try:
     from fontTools.ttLib import TTFont
@@ -64,6 +89,7 @@ def get_args(add_ignore_gooey=True):
     output_args.add_argument('--japanese36', action='store_true', help='automatically generate the 36 font for MM+')
     output_args.add_argument('-id', '--font_id', default=0, help='specify which inner font id to use when building the json')
     output_args.add_argument('-c', '--charlist', default=joinpath('misc', 'charlist.txt'), help='path to charlist file to use (default: {})'.format(joinpath('misc', 'charlist.txt')))
+    output_args.add_argument('--automate', action='store_true', default=None, help='automate font creation')
     font_args = parser.add_argument_group('Per-Font Settings', 'set the fonts to use -- all arguments accept comma-separated lists for fallback font support')
     font_args.add_argument('-f', '--font', default=None, help='source font file(s)')
     font_args.add_argument('-v', '--variation', default=None, help='name of font variation(s) to use (optional)')
@@ -408,6 +434,11 @@ for char in charlist:
 
 if args.latin9_bold36 or args.latin9 or args.japanese_bold36 or args.japanese36:
     pil_image.save('{}.png'.format(args.output_name[0]), 'PNG')
+elif args.automate:
+    pil_image.save('font11_36x36.png', 'PNG')
+    pil_image.save('font14_36x36.png', 'PNG')
+    pil_image.save('font20_36x36.png', 'PNG')
+    pil_image.save('font22_36x36.png', 'PNG')
 else:
     pil_image.save('{}.png'.format(args.output_name), 'PNG')
 
@@ -428,6 +459,93 @@ out_font = {
 if args.latin9_bold36 or args.latin9 or args.japanese_bold36 or args.japanese36:
     with open('{}.json'.format(args.output_name[0]), 'w') as f:
         json.dump(out_font, f, indent=4)
+elif args.automate:
+    font11 = {"id": 20}
+    font14 = {"id": 15}
+    font20 = {"id": 21}
+    font22 = {"id": 17}
+    with open('font11_36x36.json', 'w') as f:
+        out_font.update(font11)
+        json.dump(out_font, f, indent=4)
+    with open('font14_36x36.json', 'w') as f:
+        out_font.update(font14)
+        json.dump(out_font, f, indent=4)
+    with open('font20_36x36.json', 'w') as f:
+        out_font.update(font20)
+        json.dump(out_font, f, indent=4)
+    with open('font22_36x36.json', 'w') as f:
+        out_font.update(font22)
+        json.dump(out_font, f, indent=4)
+    if replace_sprite_texture:
+        replace_sprite_texture("base_spr_fnt_36latin9.farc", "spr_fnt_36latin9.bin", "NOMERGE_D5COMP_000_0", "font11_36x36.png", "spr_fnt_36latin9.farc")
+        replace_sprite_texture("base_spr_fnt_36.farc", "spr_fnt_36.bin", "NOMERGE_D5COMP_000_0", "font20_36x36.png", "spr_fnt_36.farc")
+        replace_sprite_texture("base_spr_fnt_bold36.farc", "spr_fnt_bold36.bin", "NOMERGE_D5COMP_000_0", "font22_36x36.png", "spr_fnt_bold36.farc")
+        replace_sprite_texture("base_spr_fnt_bold36latin9.farc", "spr_fnt_bold36latin9.bin", "NOMERGE_D5COMP_000_0", "font14_36x36.png", "spr_fnt_bold36latin9.farc")
+        exit(1)
+        var_replace_sprite = True
+    try:
+        os.replace("font11_36x36.json", "fontmap/font11_36x36.json")
+        os.replace("font14_36x36.json", "fontmap/font14_36x36.json")
+        os.replace("font20_36x36.json", "fontmap/font20_36x36.json")
+        os.replace("font22_36x36.json", "fontmap/font22_36x36.json")
+    except OSError as error:
+        print(error)
+        print("Unable to copy the fontmaps into the fontmap directory!")
+        exit(1)
+    print("Successfully moved fontmaps into the fontmap directory!")
+    # generate fontmap
+    os.system("python fontmap_extract.py fontmap/")
+    folder_name = "{}".format(font['pil_font'].getname()[0])
+    rom_folder_name = folder_name + "/rom"
+    auth2d_folder_name = rom_folder_name + "/2d"
+    if var_replace_sprite == True:
+        print("Cleaning up images..")
+        os.remove("font11_36x36.png")
+        os.remove("font14_36x36.png")
+        os.remove("font20_36x36.png")
+        os.remove("font22_36x36.png")
+    toml_array = """enabled = true
+name = "{} Font"
+description = "This is a mod that replaces both the LATIN9 and default (JP) fontsets. Does not include support for bold fonts at this time."
+version = "1.0"
+date = "22.07.2022"
+author = "feat_jinsoul"
+include = ["."]""".format(font['pil_font'].getname()[0])
+    try:
+        if not os.path.isdir(folder_name):
+            os.makedirs(folder_name)
+            os.makedirs(rom_folder_name)
+            os.makedirs(auth2d_folder_name)
+    except OSError as e:
+        print(e)
+        print("Unable to create the directories for the folder name you want. Exiting...")
+        exit(1)
+    try:
+        os.rename("spr_fnt_36latin9.farc", auth2d_folder_name + "/spr_fnt_36latin9.farc")
+        os.rename("spr_fnt_36.farc", auth2d_folder_name + "/spr_fnt_36.farc")
+        os.rename("spr_fnt_bold36.farc", auth2d_folder_name + "/spr_fnt_bold36.farc")
+        os.rename("spr_fnt_bold36latin9.farc", auth2d_folder_name + "/spr_fnt_bold36latin9.farc")
+    except OSError as e:
+        print(e)
+        print("Unable to move the sprites to their location. Exiting...")
+        exit(1)
+    # ok time to move the fontmap
+    try:
+        os.rename("fontmap.farc", rom_folder_name + "/fontmap.farc")
+    except OSError as e:
+        print(e)
+        print("Unable to move the fontmap to its location. Exiting...")
+        exit(1)
+    # ok time to. um. make the json and the toml
+    try:
+        with open("{}/config.toml".format(folder_name), "a+", encoding="utf-8") as f:
+            f.write(toml_array)
+        with open("{}/mod.json".format(folder_name), "a+", encoding="utf-8") as f:
+            f.write(json_data)
+    except OSError as e:
+        print(e)
+        print("Unable to create the config.toml. Exiting...")
+        exit(1)
 else:
     with open('{}.json'.format(args.output_name), 'w') as f:
         json.dump(out_font, f, indent=4)
